65341194a47c70ccdfa315c49dac2e9f6c7fb41d
nsolotshi <nsolotshi3@gmail.com>
2025-10-22 17:08:00 +0100

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,29 +1,18 @@
-const CACHE_NAME = 'coopec-kwilu-v1.0.1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'coopec-kwilu-v1.0.0';
 
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  console.log('Service Worker installing');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating');
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
   );
 });
