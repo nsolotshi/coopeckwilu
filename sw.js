a43238aa1e4265dc12c0e7dd1b9915e09bbe767d
nsolotshi <nsolotshi3@gmail.com>
2025-10-22 16:51:14 +0100

    Update service worker cache version and cleanup
    Updated cache version and removed activation event handling.

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'coopec-kwilu-v1.0.0';
+const CACHE_NAME = 'coopec-kwilu-v1.0.1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json',
-  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
+  '/manifest.json'
 ];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -25,19 +25,5 @@ self.addEventListener('fetch', function(event) {
         return fetch(event.request);
       }
     )
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
